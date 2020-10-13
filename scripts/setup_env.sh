b703bd08bfaa1d644185754c1ea8d83fc7946071
David Beck <41214131+dbeck6@users.noreply.github.com>
2020-10-12 19:12:31 -0500

    Add setup scripts

---
+++ scripts/setup_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install python3-pip
+sudo apt-get update
+sudo apt-get install python3.7-dev
+
+pip3 install numpy
+pip3 install pandas
+pip3 install boto3
+pip3 install tekore
+pip3 install hnswlib
+pip3 install spotipy
